d08dec1bf2b13a1bf6ca11c7901c87b181192dcd
Alex Moore <mooreless1234@gmail.com>
2024-08-12 22:09:02 -0500

    final update

--- app/sqlHelper.py
+++ app/sqlHelper.py
@@ -10,11 +10,7 @@ class SQLHelper():
 
 
     def __init__(self):
-<<<<<<< HEAD
         self.engine = create_engine("sqlite:///aqi.sqlite") # might need to change
-=======
-        self.engine = create_engine("sqlite:///../Resources/aqi.sqlite") # might need to change
->>>>>>> main
 
     
     def get_table(self, country):
@@ -41,19 +37,13 @@ class SQLHelper():
             ORDER BY
                 aqi_value DESC,
                 co_aqi_value DESC,
-<<<<<<< HEAD
                 no2_aqi_value;
-=======
-                no2_aqi_value
-            LIMIT 15;
->>>>>>> main
         """
 
         # execute query
         df = pd.read_sql(text(query), con = self.engine)
         data = df.to_dict(orient="records")
         return(data)
-<<<<<<< HEAD
         
     
     def get_map(self): #add
@@ -61,20 +51,6 @@ class SQLHelper():
         query = f"""
             SELECT latitude, longitude, aqi_value, city, country, aqi_category
             FROM aqi;
-=======
-    
-    def get_map(self, country): #add
-        
-        if country == 'All':
-            where_clause = "1=1"
-        else:
-            where_clause = f" country = '{country}'"
-         
-        query = f"""
-            SELECT latitude, longitude, aqi_value
-            FROM aqi
-            {where_clause};
->>>>>>> main
         """
         # execute query
         df = pd.read_sql(text(query), con = self.engine)
@@ -83,7 +59,6 @@ class SQLHelper():
 
 
 
-<<<<<<< HEAD
     def get_bar(self, country):
 
         # switch on user_country
@@ -113,41 +88,16 @@ class SQLHelper():
                 country DESC;
         """
 
-=======
-    def get_bar(self, country):# add
-        if country == 'All':
-            where_clause = ""
-        else:
-            where_clause = f"WHERE country = '{country}'"
-
-        query = f"""
-            SELECT
-                aqi_category,
-                COUNT(*) as count
-            FROM
-                aqi
-            {where_clause}
-            GROUP BY
-                aqi_category;
-        """
-
-        # execute query
->>>>>>> main
         df = pd.read_sql(text(query), con = self.engine)
         data = df.to_dict(orient="records")
         return(data)
 
     
-<<<<<<< HEAD
     def get_bar2(self, country):# add
-=======
-    def get_bar2(self, country): #add
->>>>>>> main
         if country == 'All':
             where_clause = ""
         else:
             where_clause = f"WHERE country = '{country}'"
-<<<<<<< HEAD
 
         query = f"""
             SELECT
@@ -165,22 +115,3 @@ class SQLHelper():
         df = pd.read_sql(text(query), con = self.engine)
         data = df.to_dict(orient="records")
         return(data)
-=======
-        query = f"""
-            SELECT
-                country,
-                city,
-                aqi_value,
-                aqi_category
-            FROM
-                aqi
-            {where_clause}
-            ORDER BY
-                aqi_value DESC
-            LIMIT
-                10;
-        """
-        df2 = pd.read_sql(text(query), con = self.engine)
-        data = df2.to_dict(orient="records")
-        return(data) 
->>>>>>> main
